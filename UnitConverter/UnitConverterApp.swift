475186512824be6a5dc08670657a0a52a3a10412
Vishal Kanojia <vishal.deep@live.com>
2022-02-27 20:24:50 +0530

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Vishal on 27/02/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
